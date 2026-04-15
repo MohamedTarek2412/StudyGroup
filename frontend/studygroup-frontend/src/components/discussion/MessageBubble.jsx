c518820e39175719f120240e78a0e8cd5afdd101
Bavly718 <167584460+Bavly718@users.noreply.github.com>
2026-04-15 19:19:58 +0200

    Update MessageBubble.jsx

--- frontend/studygroup-frontend/src/components/discussion/MessageBubble.jsx
+++ frontend/studygroup-frontend/src/components/discussion/MessageBubble.jsx
@@ -1,1 +1,8 @@
- 
+export default function MessageBubble({ message }) {
+  return (
+    <div style={{ marginBottom: "10px" }}>
+      <strong>{message.user}:</strong>
+      <p>{message.content}</p>
+    </div>
+  );
+} 
